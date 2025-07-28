32abd63306730356f393900a6624b165a66001d1
Fernando Manjón-Cabeza Gómez <116214571+fmangom292@users.noreply.github.com>
2025-07-28 09:02:07 +0000

    Agregar modelos y migraciones para las entidades People, Planet, Vehicles y Favorites con sus relaciones (Ejercicio)

--- src/models.py
+++ src/models.py
@@ -4,16 +4,88 @@ from sqlalchemy.orm import Mapped, mapped_column
 
 db = SQLAlchemy()
 
+
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    email: Mapped[str] = mapped_column(
+        String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
-
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
             # do not serialize the password, its a security breach
+        }
+
+
+class People(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    birth_year: Mapped[int]
+    eye_color: Mapped[str] = mapped_column(String(120))
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "birth_year": self.birth_year,
+            "eye_color": self.eye_color
+        }
+
+
+class Planet(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    climate: Mapped[str] = mapped_column(String(120))
+    terrain: Mapped[str] = mapped_column(String(120))
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "climate": self.climate,
+            "terrain": self.terrain
+        }
+
+
+class Vehicles(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    model: Mapped[str] = mapped_column(String(120))
+    manufacturer: Mapped[str] = mapped_column(String(120))
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "model": self.model,
+            "manufacturer": self.manufacturer
+        }
+
+
+class Favorites(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(
+        db.ForeignKey('user.id'), nullable=False)
+    people_id: Mapped[int] = mapped_column(
+        db.ForeignKey('people.id'), nullable=True)
+    planet_id: Mapped[int] = mapped_column(
+        db.ForeignKey('planet.id'), nullable=True)
+    vehicle_id: Mapped[int] = mapped_column(
+        db.ForeignKey('vehicles.id'), nullable=True)
+
+    user = db.relationship('User', backref='favorites')
+    people = db.relationship('People', backref='favorites')
+    planet = db.relationship('Planet', backref='favorites')
+    vehicle = db.relationship('Vehicles', backref='favorites')
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "people_id": self.people_id,
+            "planet_id": self.planet_id,
+            "vehicle_id": self.vehicle_id
         }
