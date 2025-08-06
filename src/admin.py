585f63df7fe4cf31c7bbf0891ad1d6f970668c7d
Fernando Manjón-Cabeza Gómez <116214571+fmangom292@users.noreply.github.com>
2025-08-06 08:21:27 +0000

    Vista personalizada de Favorites

--- src/admin.py
+++ src/admin.py
@@ -4,6 +4,11 @@ from models import db, User, People, Planet, Favorites, Vehicles
 from flask_admin.contrib.sqla import ModelView
 
 
+class FavoritesAdmin(ModelView):
+    column_list = ('id', 'user_id', 'people_id', 'planet_id', 'vehicle_id')
+    form_columns = ('user_id', 'people_id', 'planet_id', 'vehicle_id')
+
+
 def setup_admin(app):
     app.secret_key = os.environ.get('FLASK_APP_KEY', 'sample key')
     app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
@@ -14,7 +19,7 @@ def setup_admin(app):
     admin.add_view(ModelView(People, db.session))
     admin.add_view(ModelView(Planet, db.session))
     admin.add_view(ModelView(Vehicles, db.session))
-    admin.add_view(ModelView(Favorites, db.session))
+    admin.add_view(FavoritesAdmin(Favorites, db.session))
 
     # You can duplicate that line to add mew models
     # admin.add_view(ModelView(YourModelName, db.session))
